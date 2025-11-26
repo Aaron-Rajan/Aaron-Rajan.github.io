ee79c1fd0befd8289d3cd85a2b66a44661cb2837
Aaron <aaronmraj@gmail.com>
2025-11-26 15:39:28 -0500

    Switched to HashRouter so GitHub pages works

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from 'react';
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
-import App from './App.tsx'
+import { HashRouter } from 'react-router-dom';
+import './index.css';
+import App from './App.tsx';
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
